9bbd52571e476d0db22f5157ef98b124516c2688
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2026-06-09 11:15:09 +0200

    new filters done

--- kulonut-fe/src/app/components/basic-information-manager-page/basic-information-manager-page.ts
+++ kulonut-fe/src/app/components/basic-information-manager-page/basic-information-manager-page.ts
@@ -4,7 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-basic-information-manager-page',
   imports: [],
   templateUrl: './basic-information-manager-page.html',
-  styleUrl: './basic-information-manager-page.scss',
+  styleUrls: [
+    '../../app.scss',
+    './basic-information-manager-page.scss'
+  ],
 })
 export class BasicInformationManagerPage {
 
